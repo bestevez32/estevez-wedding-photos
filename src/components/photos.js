4b20aa16ac727eaca88fddec91535dda42c6fc68
bestevez32 <bestevez32@gmail.com>
2020-12-10 15:37:10 -0800

    Image cleanup

--- src/components/photos.js
+++ src/components/photos.js
@@ -11,9 +11,7 @@ import img11 from '../assets/images/wedding_11.jpg'
 import img12 from '../assets/images/wedding_12.jpg'
 import img13 from '../assets/images/wedding_13.jpg'
 import img14 from '../assets/images/wedding_14.jpg'
-// import img15 from '../assets/images/wedding_15.jpg'
 import img16 from '../assets/images/wedding_16.jpg'
-// import img17 from '../assets/images/wedding_17.jpg'
 import img18 from '../assets/images/wedding_18.jpg'
 import img19 from '../assets/images/wedding_19.jpg'
 import img20 from '../assets/images/wedding_20.jpg'
@@ -46,7 +44,7 @@ export const galleryPhotos = [
   },
   {
     src: img7,
-    title: 'Now I do not know where I am'
+    title: 'And I do not know where I am,'
   },
   {
     src: img8,
@@ -56,53 +54,41 @@ export const galleryPhotos = [
     src: img9,
     title: 'But I know where I want to go'
   },
-   {
+  {
     src: img11,
     title: 'And so I thought I would let you know'
   },
-  {
+   {
     src: img12,
-    title: 'That these things take forever'
+    title: 'Yeah, these things take forever,'
   },
   {
     src: img13,
     title: 'I especially am slow'
   },
-  // {
-  //   src: img15,
-  //   width: 1,
-  //   height: 1,
-  //   title: 'and I wondered if I could come home'
-  // },
-  // {
-  //   src: img17,
-  //   width: 1,
-  //   height: 1,
-  //   title: 'I could go anywhere with you,'
-  // },
   {
     src: img19,
-    title: 'and I would be happy'
-  },
-  {
-    src: img20,
-    title: 'and I would be happy'
-  },
-  {
-    src: img21,
-    title: 'and I would be happy'
-  },
-  {
-    src: img16,
-    title: 'And you said, "This is the first day of my life"'
-  },
-  {
-    src: img14,
     title: 'But I realized that I need you'
   },
   {
+    src: img20,
+    title: 'And I wondered if I could come home'
+  },
+  {
+    src: img21,
+    title: 'And you said'
+  },
+  {
+    src: img16,
+    title: '"This is the first day of my life'
+  },
+  {
+    src: img14,
+    title: 'I could go anywhere in this whole world with you'
+  },
+  {
     src: img18,
-    title: 'and I would be happy'
+    title: 'And I would be happy"'
   },
 ]
 
@@ -111,37 +97,31 @@ export const pageOnePhotos = [
       src: img1,
       width: 3,
       height: 4,
-      title: 'This is the first day of my life'
     },
     {
       src: img2,
       width: 7,
       height: 4,
-      title: 'Swear I was born right in the doorway'
     },
     {
       src: img3,
       width: 7,
       height: 4,
-      title: 'I went out in the rain, suddenly everything changed'
     },
     {
       src: img4,
       width: 6,
       height: 4,
-      title: 'We spread our jackets on the beach'
     },
     {
       src: img5,
       width: 4,
       height: 4,
-      title: 'Yours is the first face that I saw'
     },
     {
       src: img6,
       width: 6,
       height: 4,
-      title: 'Think I was blind before I met you'
     },
   ];
 
@@ -150,31 +130,26 @@ export const pageOnePhotos = [
       src: img7,
       width: 7,
       height: 4,
-      title: 'Now I do not know where I am'
     },
     {
       src: img8,
       width: 7,
       height: 4,
-      title: 'Or where I have been'
     },
     {
       src: img9,
       width: 3,
       height: 4,
-      title: 'But I know where I want to go'
     },
      {
       src: img11,
       width: 6,
       height: 4,
-      title: 'And so I thought I would let you know'
     },
     {
       src: img12,
       width: 6,
       height: 4,
-      title: 'That these things take forever'
     },
     {
       src: img13,
@@ -189,50 +164,26 @@ export const pageOnePhotos = [
       src: img19,
       width: 6,
       height: 4,
-      title: 'and I would be happy'
     },
     {
       src: img20,
       width: 6,
       height: 4,
-      title: 'and I would be happy'
     },
     {
       src: img21,
       width: 6,
       height: 4,
-      title: 'and I would be happy'
     },
-
-    // {
-    //   src: img15,
-    //   width: 1,
-    //   height: 1,
-    //   title: 'and I wondered if I could come home'
-    // },
-    // {
-    //   src: img16,
-    //   width: 6,
-    //   height: 4,
-    //   title: 'And you said, "This is the first day of my life"'
-    // },
-    // {
-    //   src: img17,
-    //   width: 1,
-    //   height: 1,
-    //   title: 'I could go anywhere with you,'
-    // },
     {
       src: img14,
       width: 8,
       height: 4,
-      title: 'But I realized that I need you'
     },
     {
       src: img18,
       width: 8,
       height: 4,
-      title: 'and I would be happy'
     },
   ];
   
