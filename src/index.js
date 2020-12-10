35065fe5a7447ef905a593c4928b0662927eb67f
bestevez32 <bestevez32@gmail.com>
2020-12-09 17:22:09 -0800

    Vmax

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <div style={{display: "flex", flexDirection: "column", alignContent: "center"}}>
       <div style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
-        <div style={{color:"white", maxWidth: '50%', fontSize: "4.0vw", marginTop: "1rem"}}>Amanda & Brandon</div>
-        <div style={{color:"white", maxWidth: '50%', fontSize: "3.0vw", marginTop: "0.5rem"}}>November 5th, 2020 </div>
+        <div style={{color:"white", maxWidth: '50%', fontSize: "4.0vmax", marginTop: "1rem"}}>Amanda & Brandon</div>
+        <div style={{color:"white", maxWidth: '50%', fontSize: "3.0vmax", marginTop: "0.5rem"}}>November 5th, 2020 </div>
       </div>
       <Page1/>
       <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between", marginTop: "0.5rem", marginBottom: "0.5rem"}}>
